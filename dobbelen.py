5d970ffd793633ee602b0fcb8c2ec3374fa8b7ac
triskattie <tristankoetsier01@outlook.com>
2025-09-16 16:53:08 +0200

    toevoegen modussen, begin van beurten

--- dobbelen.py
+++ dobbelen.py
@@ -1,5 +1,6 @@
 import random
 from gedeelde_functies import verkrijg_nummer
+from InquirerPy import inquirer
 
 def rol_dobbelsteen(hoeveelheid_stenen):
     uitslag = []
@@ -10,19 +11,56 @@ def rol_dobbelsteen(hoeveelheid_stenen):
 def print_scorebord(scores):
     print("SCOREBORD")
     for speler in scores:
-        print(f"{scores[speler].key()}: {scores[speler]}")
+        print(f"{speler}: {scores[speler]}")
 
+def start():
+    print("""Welkom bij mijn dobbelspel!\nMet hoeveel mensen ben je? """)
+    spelers = verkrijg_nummer(min_toegestaan=False, decimalen_toegestaan=False)
+    spel_modus = inquirer.select(
+        message="Welke spelmodus wil je gebruiken?",
+        choices=["Race naar 50", "Race naar 100", "5 rondes", "10 rondes"],
+    ).execute()
+    return spelers, spel_modus
+
+def initialiseer_spelers(n):
+    scores = {}
+    for i in range(n):
+        scores[f"Speler {n + 1}"] = 0
+
+def check():
+    pass
+
+def beurt():
+    beurt_keuze = inquirer.select(
+        message="Welke actie wil je doen?",
+        choices=["Dobbelen", "Challenge"],
+    ).execute()
+    if beurt_keuze == "Dobbelen":
+        # HIER VERDER
+
+def loop(spelers):
+    pass
+
+def race_modus(spelers, spel_modus):
+    doel = spel_modus.strip()[-1]
+    scores = initialiseer_spelers(spelers)
+    while max(scores) < doel:
+        scores = loop(scores)
+    winnaar = max(scores)
+    return winnaar
+
+def rondes_modus():
+    # HIER MOET CODE KOMEN VOOR DE RONDES MODUS
+    pass
 
 def main():
     while True:
-        print("""Welkom bij mijn dobbelspel!\nMet hoeveel mensen ben je? """)
-        spelers = verkrijg_nummer(min_toegestaan=False, decimalen_toegestaan=False)
-        print("Hoeveel rondes wil je doen?")
-        rondes = verkrijg_nummer(min_toegestaan=False, decimalen_toegestaan=False)
-        scores = {}
-        for speler in spelers:
-            scores[f"Speler {speler + 1}": 0]
-        print_scorebord()
+        spelers, spel_modus = start()
+        if spel_modus.split()[0] == "Race":
+            race_modus(spelers, spel_modus)
+        
+        if not spel_modus.split()[0].isalpha():
+            rondes_modus()
 
         
 
