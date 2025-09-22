7d320574848bbe7a4b58c71b4753aab9a888e966
triskattie <tristankoetsier01@outlook.com>
2025-09-22 18:49:45 +0200

    inzetten, en begin van het beurt systeem

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -1,14 +1,27 @@
 import hashlib
 import os
 import json
+from json import JSONDecodeError
 
-data_path = os.path.join(os.path.dirname(__file__), "data.json")
-with open(data_path, 'r') as f:
-    data = json.load(f)
+from gedeelde_functies import verkrijg_nummer
+import random
 
 def save_data(data):
     with open(data_path, 'w') as f:
         json.dump(data, f, indent=4)
+
+data_path = os.path.join(os.path.dirname(__file__), "data.json")
+try:
+    with open(data_path, 'r') as f:
+        data = json.load(f)
+except JSONDecodeError:
+    data = {
+        "Logins": {},
+        "Balances": {}
+    }
+    save_data(data)
+
+
 
 def hash_passwd(password):
     return hashlib.sha512(password.encode('utf-8')).hexdigest()
@@ -51,15 +64,50 @@ def user_selection():
                     points = data["Balances"][username]
                     return username, points
 
-def betting():
+def betting(limit):
+    while True:
+        print("How many points would you like to bet? ")
+        amount = verkrijg_nummer(min_toegestaan=False, decimalen_toegestaan=True)
+        if amount > limit:
+            print(f"You don't have enough points! You have {limit} points.")
+            continue
+        return amount
+
+def beurt_keuze(player_cards, points):
+    if len(player_cards) == 2:
+        double = True
+        if player_cards[0] == player_cards[1]:
+            split_possible = True
+
+def print_cards(computer_cards, player_cards):
+    computer_cards_string = "  ".join(computer_cards)
+    print("COMPUTER CARDS")
+    print(computer_cards_string)
+    player_cards_string = "  ".join(player_cards)
+    print("PLAYER CARDS")
+    print(player_cards_string)
+    while True:
 
 
+def game_round(username, points):
+    cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "Joker"]
+    computer_cards = []
+    for i in range(2):
+        computer_cards.append(random.choice(cards))
+    player_cards = []
+    for i in range(2):
+        player_cards.append(random.choice(cards))
+
+    print_cards(computer_cards, player_cards)
+    return
 
 
 def main():
     username, points = user_selection()
     while True:
-        pass
+        bet = betting(points)
+        points = game_round(username, points)
+        save_data(points)
 
 
 if __name__ == "__main__":
